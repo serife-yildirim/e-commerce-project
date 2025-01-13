1883ea062a2e9760f97788c2dc6015f22caa824c
serife-yildirim <serifeozyurek-94@hotmail.com>
2025-01-13 16:06:47 +0300

    Temiz kurulum sonrası lock dosyası eklendi

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Build çıktısı için hedef klasör
+    sourcemap: true, // Debug için source map ekler
+  }
+});
