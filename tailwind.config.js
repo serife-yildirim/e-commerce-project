2fd155025a8b2290e2e17ed83b728bf1e79b8e07
serife-yildirim <serifeozyurek-94@hotmail.com>
2025-01-14 13:41:26 +0300

    Add ProductCard component and configure Tailwind utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,37 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+// tailwind.config.js
+import plugin from 'tailwindcss/plugin'
+
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        
+        Primary: '#23A6F0',
+        Secondary: '#23856D',
+        alert: '#E77C40',
+        darkBackground: '#252B42',
+        textColor: '#252B42',
+        SecondaryTextColor: '#737373',
+        muted: '#999999',
+      },
+      height: {
+        
+        cardBestceller: '425px',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addUtilities }) {
+   
+      const newUtilities = {
+        '.h6': { fontSize: '16px' },
+        '.h7': { fontSize: '14px' },
+      }
+      addUtilities(newUtilities)
+    }),
+  ],
 }
