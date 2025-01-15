bd3a32b4051559c398b0e2d1d14c9ca53ad7cb85
serife-yildirim <serifeozyurek-94@hotmail.com>
2025-01-15 18:20:16 +0300

    Fix App.jsx structure with proper tag closure for Router, Switch, and Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Header from './layout/Header'
-import HomePage from './Pages/HomePage';
-
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Header from "./layout/Header";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
@@ -13,8 +11,7 @@ function App() {
       <Header />
       <div>
         <Switch>
-        <Route exact path="/" component={HomePage} />
-          </Route>
+          <Route exact path="/" component={HomePage} />
         </Switch>
         <ToastContainer />
       </div>
