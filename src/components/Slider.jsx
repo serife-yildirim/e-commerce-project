6f78152cc54f7f56423837a0a136f7a6fc57ab34
serife-yildirim <serifeozyurek-94@hotmail.com>
2025-01-16 13:04:44 +0300

    Update Slider component and remove container config

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,12 +2,16 @@ import React from "react"
 function Slider() {
   
     return (
-      <div className="bg-[url('/images/homeHero.jpg')] bg-no-repeat bg-cover bg-top text-lightTextColor flex justify-center items-center  ">
-      <div className="flex flex-col items-center gap-12 py-44 max-w-64 ">
-        <h6 className="h6 text-base font-bold">SUMMER 2020</h6>
-        <h2 className="h2 text-4xl font-bold">NEW COLLECTION</h2>
-        <h4 className="text-xl text-lightGray">We know how large objects will act, but things on a small scale.</h4>
-        <button className="bg-succes text-lightTextColor text-3xl font-bold w-56 h-16 rounded-md">SHOP NOW</button>
+      <div className="bg-[url('/images/homeHero.jpg')] bg-no-repeat bg-cover bg-top text-lightTextColor flex justify-center items-center sm:items-start sm:justify-start sm:px-40 ">
+      <div className="flex flex-col items-center sm:items-start gap-12 py-44 max-w-64 sm:max-w-none">
+        <h6 className="h6 font-bold">SUMMER 2020</h6>
+        <h2 className="h2 sm:h1 font-bold">NEW COLLECTION</h2>
+        <h4 className="h4 sm:h5 sm:max-w-80 text-lightGray">
+          We know how large objects will act, but things on a small scale.
+        </h4>
+        <button className="bg-succes text-lightTextColor text-3xl font-bold w-56 h-16 rounded-md">
+          SHOP NOW
+        </button>
       </div>
       </div>
     )
