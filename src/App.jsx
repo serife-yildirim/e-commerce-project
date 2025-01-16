d9cc14ad17a7cf0cc26b7b38e694d11a86dca89f
serife-yildirim <serifeozyurek-94@hotmail.com>
2025-01-16 12:51:20 +0300

    remove duplicate Header

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Header from "./layout/Header";
 import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
     <Router>
-      <Header />
       <div>
         <Switch>
           <Route exact path="/" component={HomePage} />
