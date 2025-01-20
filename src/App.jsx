308bd2ea4884500fbcb9ba136c5ef2686d19fc44
serife-yildirim <serifeozyurek-94@hotmail.com>
2025-01-20 13:22:02 +0300

    Add Signup and Login routes to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import HomePage from "./Pages/HomePage";
+import SignupForm from "./Pages/SignupForm";
+import LoginForm from "./Pages/LoginForm";
+
 
 function App() {
   return (
     <Router>
       <div>
         <Switch>
+        <Route path="/signup" component={SignupForm} />
+        <Route path="/login" component={LoginForm} />
           <Route exact path="/" component={HomePage} />
         </Switch>
         <ToastContainer />
